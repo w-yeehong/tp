aacfca950b066b97cf82bec78554c79a5cfcab3c
WaiLok <e0323018@u.nus.edu>
2020-09-26 12:46:22 +0800

    no message

--- src/test/java/seedu/address/testutil/SearchPatientDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/SearchPatientDescriptorBuilder.java
@@ -1,2 +1,39 @@
-package seedu.address.testutil;public class SearchPatientDescriptorBuilder {
+package seedu.address.testutil;
+
+import seedu.address.logic.commands.SearchPatientCommand.SearchPatientDescriptor;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.TemperatureRange;
+
+/**
+ * A utility class to help with building SearchPatientDescriptor objects.
+ */
+public class SearchPatientDescriptorBuilder {
+
+    private SearchPatientDescriptor descriptor;
+
+    public SearchPatientDescriptorBuilder() {
+        descriptor = new SearchPatientDescriptor();
+    }
+
+
+    /**
+     * Sets the {@code Name} of the {@code SearchPatientDescriptor} that we are building.
+     */
+    public SearchPatientDescriptorBuilder withName(String name) {
+        descriptor.setName(new Name(name));
+        return this;
+    }
+
+
+    /**
+     * Sets the {@code Temperature} of the {@code SearchPatientDescriptor} that we are building.
+     */
+    public SearchPatientDescriptorBuilder withTemperatureRange(String temperatureRange) {
+        descriptor.setTemperatureRange(new TemperatureRange(temperatureRange));
+        return this;
+    }
+
+    public SearchPatientDescriptor build() {
+        return descriptor;
+    }
 }
