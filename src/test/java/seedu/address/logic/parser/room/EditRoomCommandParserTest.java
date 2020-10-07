ca269b68b986dcd0cfaca05eb287e6207e9c7468
Lee Ming De <leemingde@gmail.com>
2020-10-07 11:19:25 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/parser/room/EditRoomCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/room/EditRoomCommandParserTest.java
@@ -1,28 +1,12 @@
 package seedu.address.logic.parser.room;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.commands.NewCommandTestUtil.AGE_DESC_AMY;
-import static seedu.address.logic.commands.NewCommandTestUtil.INVALID_AGE_DESC;
-import static seedu.address.logic.commands.NewCommandTestUtil.INVALID_NAME_DESC;
-import static seedu.address.logic.commands.NewCommandTestUtil.INVALID_PERIOD_DESC;
-import static seedu.address.logic.commands.NewCommandTestUtil.INVALID_PHONE_DESC;
-import static seedu.address.logic.commands.NewCommandTestUtil.INVALID_TEMP_DESC;
-import static seedu.address.logic.commands.NewCommandTestUtil.PHONE_DESC_BOB;
-import static seedu.address.logic.commands.NewCommandTestUtil.VALID_NAME_AMY;
-import static seedu.address.logic.commands.NewCommandTestUtil.VALID_NAME_BOB;
-import static seedu.address.logic.commands.NewCommandTestUtil.VALID_PERIOD_AMY;
-import static seedu.address.logic.commands.NewCommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.commands.room.EditRoomCommand.MESSAGE_ROOM_NOT_EDITED;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.room.EditRoomCommand;
-import seedu.address.model.patient.Age;
-import seedu.address.model.patient.Name;
-import seedu.address.model.patient.PeriodOfStay;
-import seedu.address.model.patient.Phone;
-import seedu.address.model.patient.Temperature;
 
 public class EditRoomCommandParserTest {
     private static final String MESSAGE_INVALID_FORMAT =
