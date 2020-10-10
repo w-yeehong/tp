f6e154d16c633404feaf6eeb29f25c70c2ef09b2
Chiam Yun Qing <chiamyunqing@gmail.com>
2020-10-10 12:21:42 +0800

    Complete skeletal code for SearchRoomCommand

---
+++ src/main/java/seedu/address/logic/parser/room/SearchRoomCommandParser.java
@@ -0,0 +1,30 @@
+package seedu.address.logic.parser.room;
+
+import seedu.address.logic.commands.room.SearchRoomCommand;
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.RoomParserUtil;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+/**
+ * Parses input arguments and creates a new SearchRoomCommand object
+ */
+public class SearchRoomCommandParser implements Parser<SearchRoomCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the SearchRoomCommand
+     * and returns a SearchRoomCommand object for execution.
+     * @throws ParseException if the user input does not conform to the expected format
+     */
+    @Override
+    public SearchRoomCommand parse(String userInput) throws ParseException {
+        try {
+            Integer roomNumber = RoomParserUtil.parseRoomNumber(userInput.trim());
+            return new SearchRoomCommand(roomNumber);
+        } catch (ParseException pe){
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    SearchRoomCommand.MESSAGE_USAGE));
+        }
+    }
+}
