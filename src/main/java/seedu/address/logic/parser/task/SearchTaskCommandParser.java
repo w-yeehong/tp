c4b77d514e2b1171e3aab296cb9beca52b6ff07a
WaiLok <e0323018@u.nus.edu>
2020-10-12 12:13:28 +0800

    add javadoc

--- src/main/java/seedu/address/logic/parser/task/SearchTaskCommandParser.java
+++ src/main/java/seedu/address/logic/parser/task/SearchTaskCommandParser.java
@@ -14,14 +14,15 @@ import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.task.DateTimeDue;
 
 /**
- * Parses the given {@code String} of arguments in the context of the SearchTaskCommand
- * and returns an SearchTaskCommand object for execution.
- *
- * @throws ParseException if the user input does not conform to the expected format.
+ * Parses input arguments and creates a new SearchTaskCommand object
  */
-
 public class SearchTaskCommandParser implements Parser<SearchTaskCommand> {
-
+    /**
+     * Parses the given {@code String} of arguments in the context of the SearchTaskCommand
+     * and returns an SearchTaskCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform to the expected format.
+     */
     public SearchTaskCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_DUE_DATE);
