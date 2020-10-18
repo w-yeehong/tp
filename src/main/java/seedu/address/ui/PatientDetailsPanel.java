f03e4ee86b95afc210fc0ffbd4412331cfeaae1e
Chiam Yun Qing <chiamyunqing@gmail.com>
2020-10-18 21:14:05 +0800

    Complete patient tab UI

---
+++ src/main/java/seedu/address/ui/PatientDetailsPanel.java
@@ -0,0 +1,69 @@
+package seedu.address.ui;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.patient.Patient;
+
+/**
+ * Panel containing the details of the patient.
+ */
+public class PatientDetailsPanel extends UiPart<Region> {
+
+    private static final String FXML = "PatientDetailsPanel.fxml";
+
+    public final Patient patient;
+
+    @FXML
+    private Label name;
+    @FXML
+    private Label phone;
+    @FXML
+    private Label temperature;
+    @FXML
+    private Label periodOfStay;
+    @FXML
+    private Label age;
+    @FXML
+    private Label comment;
+
+    /**
+     * Creates a {@code PatientDetailsPanel} with the given {@code Patient}.
+     */
+    public PatientDetailsPanel(Patient patient) {
+        super(FXML);
+        this.patient = patient;
+        setPatientDetails();
+    }
+
+    /**
+     * Sets the details of the patient.
+     *
+     */
+    private void setPatientDetails() {
+        name.setText(patient.getName().toString());
+        temperature.setText("Temperature: " + patient.getTemperature().toString());
+        periodOfStay.setText("Period of stay: " + patient.getPeriodOfStay().toString());
+        phone.setText("Phone number: " + patient.getPhone().value);
+        age.setText("Age: " + patient.getAge().toString());
+        comment.setText("Comment: " + patient.getComment().toString());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+        // instanceof handles nulls
+        if (!(other instanceof PatientDetailsPanel)) {
+            return false;
+        }
+        // state check
+        PatientDetailsPanel panel = (PatientDetailsPanel) other;
+        return patient.equals(panel.patient);
+    }
+}
--- src/main/java/seedu/address/ui/PatientListPanel.java
+++ src/main/java/seedu/address/ui/PatientListPanel.java
@@ -6,6 +6,8 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.patient.Patient;
@@ -17,8 +19,13 @@ public class PatientListPanel extends UiPart<Region> {
     private static final String FXML = "PatientListPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(PatientListPanel.class);
 
+    private PatientDetailsPanel patientDetailsPanel;
+
     @FXML
     private ListView<Patient> patientListView;
+
+    @FXML
+    private AnchorPane patientDetailsPanelPlaceholder;
 
     /**
      * Creates a {@code PatientListPanel} with the given {@code ObservableList}.
@@ -30,13 +37,24 @@ public class PatientListPanel extends UiPart<Region> {
     }
 
     /**
+     * Handles mouse click event on the patient card.
+     *
+     * @param mouseEvent created by the user
+     */
+    @FXML
+    public void handleMouseClick(MouseEvent mouseEvent) {
+        Patient patientToDisplay = patientListView.getSelectionModel().getSelectedItem();
+        patientDetailsPanel = new PatientDetailsPanel(patientToDisplay);
+        patientDetailsPanelPlaceholder.getChildren().add(patientDetailsPanel.getRoot());
+    }
+
+    /**
      * Custom {@code ListCell} that displays the graphics of a {@code Patient} using a {@code PatientCard}.
      */
     class PatientListViewCell extends ListCell<Patient> {
         @Override
         protected void updateItem(Patient patient, boolean empty) {
             super.updateItem(patient, empty);
-
             if (empty || patient == null) {
                 setGraphic(null);
                 setText(null);
@@ -45,5 +63,4 @@ public class PatientListPanel extends UiPart<Region> {
             }
         }
     }
-
 }
